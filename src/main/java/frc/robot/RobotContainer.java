13e7baa84f636fb72f1fc50a144f0102eaa3ee8b
frankieOH <olivefhenry@gmail.com>
2026-02-28 20:27:43 -0700

    all current working changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,7 +29,7 @@ import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Command.ShooterFeederCommand;
 import frc.robot.generated.Constants;
-import frc.robot.generated.TunerConstantsSwerve;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.FeederSubystem;
 import frc.robot.subsystems.Intake;
@@ -40,11 +40,9 @@ import frc.robot.subsystems.Indexerx44;
 //import frc.robot.subsystems.IndexerNeo;
 
 public class RobotContainer {
-    private double MaxSpeed = TunerConstantsSwerve.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired
-                                                                                              // top speed
-    private double MaxAngularRate = RotationsPerSecond.of(.75).in(RadiansPerSecond); // 3/4 of a rotation per
-                                                                                           // second max angular
-                                                                                           // velocity
+    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
     private final SendableChooser<Command> autoChooser;
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
